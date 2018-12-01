0c04c52e5d3180586258e004ac8621925295e1dc
Thomas <thomascaycedo19@outlook.com>
2018-11-30 21:00:11 -0500

    Se crea una alerta, se le añaden los botones y validacion

--- src/pages/pendientes/pendientes.component.ts
+++ src/pages/pendientes/pendientes.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { TareasService } from '../../app/services/tareas.service';
 import { Lista } from '../../models/lista.model';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 import { AgregarPage } from '../agregar/agregar.component';
 
 
@@ -16,7 +16,8 @@ import { AgregarPage } from '../agregar/agregar.component';
 export class PendientesPage {
 
     constructor( public tareasService: TareasService,
-                private navCtrl: NavController){
+                 private navCtrl: NavController,
+                 private alertaCtrl: AlertController){
 
     }
     listaSeleccionada(lista:Lista){ 
@@ -24,9 +25,29 @@ export class PendientesPage {
         
     }
     agregarLista(){
-            
 
-        this.navCtrl.push(AgregarPage);
+      const alerta = this.alertaCtrl.create({
+          title: 'Nueva Lista',
+          message: 'Nombre de la nueva lista que se desea',
+          inputs:[{
+              name:'titulo',
+              placeholder: 'Nombre de la lista'
+          }],
+          buttons: [{
+              text: 'Cancelar'
+          },{
+              text: 'Agregar',
+              handler: data =>{
+                  if(data.titulo.length === 0){
+                      return;
+                  }
+                  this.navCtrl.push(AgregarPage,{
+                      titulo: data.titulo
+                  });
+              }
+          }]
+      });
+      alerta.present();
     }
 
 }
